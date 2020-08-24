754646ede94f71663cdf99c9302a068a0e2baf36
Flávio Santana <9090flavio@gmail.com>
2020-08-24 20:51:45 -0300

    [JAVA E JPA: PERSISTA SEUS OBJETOS COM A JPA2 E HIBERNATE][INTRODUÇÃO À JAVA PERSISTENCE API][CRIAÇÃO DAS TABELAS]

--- src/main/java/br/com/alura/model/Conta.java
+++ src/main/java/br/com/alura/model/Conta.java
@@ -1,6 +1,8 @@
 package br.com.alura.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
@@ -8,6 +10,7 @@ public class Conta {
 
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private Integer agencia;
