42d5922d840d248adbecbec4348e71d3ecb5d2df
Flávio Santana <9090flavio@gmail.com>
2020-08-25 09:57:56 -0300

    [JAVA E JPA: PERSISTA SEUS OBJETOS COM A JPA2 E HIBERNATE][RELACIONAMENTO PARA MUITOS][RELACIONAMENTO UM PARA UM]

--- src/main/java/br/com/alura/model/Cliente.java
+++ src/main/java/br/com/alura/model/Cliente.java
@@ -1,9 +1,6 @@
 package br.com.alura.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Cliente {
@@ -11,6 +8,9 @@ public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @OneToOne
+    private Conta conta;
 
     private String nome;
 
