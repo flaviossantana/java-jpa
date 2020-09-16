b848f9ab03cff16628513914f545e943b0f2334d
Flávio Santana <9090flavio@gmail.com>
2020-09-15 21:32:03 -0300

    [JAVA E JPA: PERSISTA SEUS OBJETOS COM A JPA2 E HIBERNATE][PESQUISANDO COM JPQL][PESQUISANDO COM JOIN]

---
+++ src/test/java/br/com/alura/MovimentacaoCategoriaTest.java
@@ -0,0 +1,38 @@
+package br.com.alura;
+
+import br.com.alura.model.Categoria;
+import br.com.alura.model.Movimento;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+public class MovimentacaoCategoriaTest {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("conta-db");
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+        StringBuilder select = new StringBuilder();
+        select.append("select m ");
+        select.append("from Movimento m ");
+        select.append("join m.categorias c ");
+        select.append("where c = :pCategoria ");
+
+        TypedQuery<Movimento> query = em.createQuery(select.toString(), Movimento.class);
+        query.setParameter("pCategoria", new Categoria(13l));
+
+        List<Movimento> movimentos = query.getResultList();
+
+        for (Movimento movimento : movimentos){
+            System.out.println("### MOVIMENTO: " + movimento);
+        }
+
+        em.getTransaction().commit();
+    }
+
+}
