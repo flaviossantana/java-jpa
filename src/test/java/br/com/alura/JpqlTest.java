cbba53185bdcbe0900fed2731edbd941e58ef29f
Flávio Santana <9090flavio@gmail.com>
2020-09-15 21:09:13 -0300

    [JAVA E JPA: PERSISTA SEUS OBJETOS COM A JPA2 E HIBERNATE][RELACIONAMENTO PARA MUITOS][TESTANDO O RELACIONAMENTO ENTRE CONTA E CLIENTE]

---
+++ src/test/java/br/com/alura/JpqlTest.java
@@ -0,0 +1,43 @@
+package br.com.alura;
+
+import br.com.alura.model.Conta;
+import br.com.alura.model.Movimento;
+import com.github.javafaker.Faker;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import java.util.List;
+
+public class JpqlTest {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("conta-db");
+        EntityManager entityManager = emf.createEntityManager();
+
+        entityManager.getTransaction().begin();
+
+        StringBuilder select = new StringBuilder();
+        select.append("select m ");
+        select.append("from Movimento m ");
+        select.append("where m.conta = :pConta ");
+
+        Query query = entityManager.createQuery(select.toString(), Movimento.class);
+        query.setParameter("pConta", new Conta(new Faker().number().numberBetween(1L, 32L)));
+
+
+        List<Movimento> movimentos = query.getResultList();
+
+
+        for (Movimento movimento: movimentos) {
+            System.out.println(movimento.toString());
+        }
+
+        entityManager.getTransaction().commit();
+        entityManager.close();
+
+    }
+
+}
